36d3221a99d547f631e285eb71aebc7f68ba0f31
Diego García <diego350z1@gmail.com>
2022-03-12 12:52:37 -0600

    Eliminación de vistas de usuarios

--- config/conexion.js
+++ config/conexion.js
@@ -3,30 +3,18 @@ var mysql = require('mysql'); //Importamos mysql
 /**
  * Objeto para la conexión con la base de datos 
  */
-<<<<<<< HEAD
 var conexion = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'agenda'
-=======
-var con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'Control_Gastos'
->>>>>>> 475c6bc4465de33a2ba4383c511f07e986c5bd39
 });
 
 
 /**
  * Nos conectamos a la base de datos
  */
-<<<<<<< HEAD
 conexion.connect(
-=======
-con.connect(
->>>>>>> 475c6bc4465de33a2ba4383c511f07e986c5bd39
     (err) => {
         if(!err) {
             console.log('Conexión establecida');
@@ -36,8 +24,4 @@ con.connect(
     }
 );
 
-<<<<<<< HEAD
 module.exports = conexion;
-=======
-module.exports = con;
->>>>>>> 475c6bc4465de33a2ba4383c511f07e986c5bd39
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-<<<<<<< HEAD
 const HomeController = require('../controllers/HomeController');
 const AgendaController = require('../controllers/AgendaController');
-
-=======
-const HomeController = require('../controllers/HomeController')
-const UsuariosController = require('../controllers/UsuariosController');
->>>>>>> 475c6bc4465de33a2ba4383c511f07e986c5bd39
 
 /* GET home page. */
 router.get('/', HomeController.index);
 router.get('/agenda', AgendaController.index);
-
-
-router.get('/users', UsuariosController.index);
 
 module.exports = router;
