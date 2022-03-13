af16c253c25389c2c7de1229ae2b3a1e0dd459ac
Diego García <diego350z1@gmail.com>
2022-03-12 19:45:16 -0600

    Modificación de ruta '/'

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-const HomeController = require('../controllers/HomeController');
 const AgendaController = require('../controllers/AgendaController');
 
 /* GET home page. */
-router.get('/', HomeController.index);
+router.get('/', AgendaController.index);
 router.get('/agenda', AgendaController.index);
 router.get('/agenda/crear', AgendaController.crear);
 router.post('/agenda', AgendaController.guardar);
